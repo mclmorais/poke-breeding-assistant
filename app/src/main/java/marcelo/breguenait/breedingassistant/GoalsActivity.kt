024ff00cc7d64623afe906c7806e46e8d6342b26
Marcelo Morais <marcelodotmorais@gmail.com>
2017-12-26 19:23:08 -0200

    Initial commit.

---
+++ app/src/main/java/marcelo/breguenait/breedingassistant/GoalsActivity.kt
@@ -0,0 +1,12 @@
+package marcelo.breguenait.breedingassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.goals_activity)
+    }
+}
